33c6cf7e40b3a2c0c5113adbf0739fdd23b23f82
Nick Kaiser <nick.j.kaiser@gmail.com>
2024-01-02 19:55:49 -0500

    Completed day 2

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/nick/Documents/Code/day_1/.venv"
+set -gx VIRTUAL_ENV "/Users/nick/Documents/Code/advent-of-code-2023/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
