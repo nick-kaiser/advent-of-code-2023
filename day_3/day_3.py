e2e4e6db1ac2000851907cd6d10b24ff8efebdbc
Nick Kaiser <nick.j.kaiser@gmail.com>
2024-01-04 21:16:54 -0500

    Completed day 3

--- day_3/day_3.py
+++ day_3/day_3.py
@@ -57,3 +57,62 @@ total_sum = sum(row_sums)
 
 print(total_sum)
 
+# Now, if there are two numbers adjacent to a * multiply the two numbers and sum all these products
+
+# First, detect the location of all *'s
+
+gear_iter = [re.finditer('[\*]', line) for line in lines]
+
+gear_list = list(map(list, gear_iter))
+
+gear_pos = []
+
+for row in gear_list:
+    if len(row) == 0:
+        gear_pos.append([])
+    else:
+        gear_indexes = []
+        for match in row:
+            gear_indexes.append(match.start())
+        gear_pos.append(gear_indexes)
+
+# Calculate valid gear products going through the rows for each gear
+
+gear_products = []
+
+for num, row in enumerate(gear_pos):
+    # If there are no gears in the row, return an empty list
+    if len(row) == 0:
+        gear_products.append([])
+    else:
+        # valid_row_nums tracks valid products associated with gears
+        valid_row_prods = []
+        for gear in row:
+            # num_adjacent tracks numbers adjacent to each gear
+            num_adjacent = []
+            # Look for matches in the prior row if row number > 0
+            if num > 0:
+                for vals in num_pos[num - 1]:
+                    if gear in vals[0]:
+                        num_adjacent.append(vals[1])
+            # Always look for matches in the current row
+            for vals in num_pos[num]:
+                if gear in vals[0]:
+                    num_adjacent.append(vals[1])
+            # Look for matches in the next row if row number < 139
+            if num < 139:
+                for vals in num_pos[num + 1]:
+                    if gear in vals[0]:
+                        num_adjacent.append(vals[1])
+            # Once adjacent numbers are identified, check if we have exactly two and calculate
+            # and append the product if we do
+            if len(num_adjacent) == 2:
+                valid_row_prods.append(num_adjacent[0] * num_adjacent[1])
+        # Append valid row prods to gear products (empty if none are found)
+        gear_products.append(valid_row_prods)
+
+gear_row_sums = [sum(gear_prods) for gear_prods in gear_products]
+
+total_gear_sum = sum(gear_row_sums)
+
+print(total_gear_sum)
